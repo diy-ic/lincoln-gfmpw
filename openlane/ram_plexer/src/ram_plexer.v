6966454b5b0fd9793e6ac18ceaaf11d1215bb328
kris j <kris.jurkans@gmail.com>
2023-12-10 15:55:05 +0000

    wip ram plexer, no wishbone support and missing WE pins

--- openlane/ram_plexer/src/ram_plexer.v
+++ openlane/ram_plexer/src/ram_plexer.v
@@ -5,32 +5,32 @@ module ram_plexer (
 `endif
     // baby io
     input baby_clk_i,
-    input [31:0] baby_addr_i,
+    input [4:0] baby_addr_i,
     input [31:0] baby_data_i,
     output reg [31:0] baby_data_o,
 
     // spi io
     input spi_clk_i, // not to be confused with the actual SPI clock provided externally
-    input [31:0] spi_addr_i,
+    input [4:0] spi_addr_i,
     input [31:0] spi_data_i,
     output reg [31:0] spi_data_o,
 
     // wishbone io
-    input wb_clk_i,
-    input [31:0] wb_addr_i,
-    input [31:0] wb_data_i,
-    output reg [31:0] wb_data_o,
+    // input wb_clk_i,
+    // input [31:0] wb_addr_i,
+    // input [31:0] wb_data_i,
+    // output reg [31:0] wb_data_o,
 
     // ram
     output reg ram_clk_o,
-    output reg [31:0] ram_addr_o,
+    output reg [4:0] ram_addr_o,
     output reg [31:0] ram_data_o,
     input [31:0] ram_data_i,
 
     // plex
     input baby_halt,
     input spi_cs,
-    input wb_config_en
+    // input wb_config_en
 );
 
     // plexing states:
@@ -58,7 +58,8 @@ assign wb_data_o = ram_data_i;
 assign spi_data_o = ram_data_i;
 
 always @ (*) begin
-    case ({wb_config_en, spi_cs, baby_halt})
+    // case ({wb_config_en, spi_cs, baby_halt})
+    case ({1'b0, spi_cs, baby_halt})
 
         // no halt
         3'b000: begin
@@ -74,12 +75,14 @@ always @ (*) begin
             ram_clk_o <= spi_clk_i;
         end
 
-        3'b101: begin
-            ram_addr_o <= wb_addr_i;
-            ram_data_o <= wb_data_i;
-            ram_clk_o <= wb_clk_i;
-        end
+        // halt + wb_config_en
+        // 3'b101: begin
+        //     ram_addr_o <= wb_addr_i;
+        //     ram_data_o <= wb_data_i;
+        //     ram_clk_o <= wb_clk_i;
+        // end
 
+        // default let baby control ram
         default: begin
             ram_addr_o <= baby_addr_i;
             ram_data_o <= baby_data_i;
