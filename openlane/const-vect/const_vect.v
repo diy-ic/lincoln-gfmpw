a0d54f3e7b8e7ef1279cd7bc8adfc58823be6bd7
kris j <kris.jurkans@gmail.com>
2023-12-11 02:20:19 +0000

    added val for io_oeb

--- openlane/const-vect/const_vect.v
+++ openlane/const-vect/const_vect.v
@@ -5,9 +5,11 @@ module constant_vectors(
 `endif
     // output [7:0] zeroes,
     // output [7:0] ones
-    output zero
+    output zero,
+    output [37:0] io_oeb_val
 );
     assign zero = 1'b0;
+    assign io_oeb_val = 37'h7BFFFFF7F;
     // assign zeroes = 8'h00;
     // assign ones = 8'hFF;
 
